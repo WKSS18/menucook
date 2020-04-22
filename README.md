664240af342bcfbfb196279149f5c764aaba10fc
WKSS18 <w1455068403@163.com>
2020-04-22 22:39:10 +0800

    'update'

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 react-cook
+
+框架：
+    使用了ant-design
+    TabBar标签栏    
+    https://mobile.ant.design/docs/react/introduce-cn
+    引入：
+    import 'antd-mobile/dist/antd-mobile.css'; 
+    import { TabBar } from 'antd-mobile';
+
+
---
+++ src/views/index/Index.jsx
@@ -0,0 +1,105 @@
+import React, { Component } from 'react'
+import 'antd-mobile/dist/antd-mobile.css';
+import { TabBar } from 'antd-mobile';
+import cookbook from 'assets/images/cookbook.png'
+import cookbookActive from 'assets/images/cookbook-active.png'
+import menu from 'assets/images/menu.png'
+import menuActive from 'assets/images/menu-active.png'
+import more from 'assets/images/more.png'
+import moreActive from 'assets/images/more-active.png'
+const CookBook = () => {
+    return <h1>cook book pages</h1>
+}
+const Category = () => {
+    return <h1>Category pages</h1>
+}
+const More = () => {
+    return <h1>More pages</h1>
+}
+
+// 菜单数据
+const tablist = [{
+    id:1,
+    title: "菜谱大全",
+    icon: cookbook,
+    iconActive: cookbookActive,
+    comp: CookBook
+},
+{
+    id:2,
+    title: "分类",
+    icon: menu,
+    iconActive: menuActive,
+    comp: Category
+},
+{
+    id:3,
+    title: "更多",
+    icon: more,
+    iconActive: moreActive,
+    comp: More
+}
+]
+
+export default class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 1,
+            hidden: false,
+            tablist
+        };
+    }
+    render() {
+        return (
+            <div style={{ position: 'fixed', height: '100%', width: '100%', top: 0 }}>
+                <TabBar
+                    unselectedTintColor="#949494"
+                    tintColor="#33A3F4"
+                    barTintColor="white"
+                    hidden={this.state.hidden}
+                >
+
+                    {
+                        this.state.tablist.map((item, index) => {
+                            return (
+                                <TabBar.Item
+                                    title={item.title}
+                                    key={item.id}
+                                    icon={<div style={{
+                                        width: '22px',
+                                        height: '22px',
+                                        background: `url(${item.icon}) center center /  21px 21px no-repeat`
+                                    }}
+                                    />
+                                    }
+                                    selectedIcon={<div style={{
+                                        width: '22px',
+                                        height: '22px',
+                                        background: `url(${item.iconActive}) center center /  21px 21px no-repeat`
+                                    }}
+                                    />
+                                    }
+                                    selected={this.state.selectedTab === item.id}
+                                    onPress={() => {
+                                        this.setState({
+                                            selectedTab: item.id,
+                                        });
+                                    }}
+                                    data-seed="logId"
+                                >
+                                    {<item.comp></item.comp>}
+                                </TabBar.Item>
+
+                            )
+                        })
+                    }
+
+
+
+
+                </TabBar>
+            </div>
+        );
+    }
+}
